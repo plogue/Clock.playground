7f42e9b2b3f3efbaa0bdc4eefc1c92a2a7f0f88c
plogue <prc-lh138-06@PRC-LH138-06s-iMac.local>
2016-07-12 09:26:06 -0500

    clock

---
+++ Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let hours = arc4random_uniform(24) + 1
+let minutes = arc4random_uniform(60) + 1
+let seconds = arc4random_uniform(60) + 1
+
+
+print("The time is: ", hours, ":", minutes, ":", seconds, terminator:"")
+
+
+
+
+
